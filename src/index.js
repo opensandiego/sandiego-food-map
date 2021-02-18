862a164acf4fca10b227d7c0396d8fa6a6dc13de
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-02-17 18:06:22 -0800

    initial sample 211 data and basic map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-alert("hello World");
+
+function init(){
+    const map = L.map('map');
+    const defaultCenter = [32.8546305,-117.051348];
+    const defaultZoom = 10;
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', 
+        {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    map.setView(defaultCenter, defaultZoom);
+
+
+}
+
+
+
+window.onload = init
