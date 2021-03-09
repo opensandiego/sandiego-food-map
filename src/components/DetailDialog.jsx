0bfce4836499373fcca31b75095f874dc7c8f8f4
FloTheFlower <florentina.hofbauer@gmail.com>
2021-03-09 11:27:34 -0800

    cleaning up

--- src/components/DetailDialog.jsx
+++ src/components/DetailDialog.jsx
@@ -17,7 +17,6 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = {
   title: {  
     flexGrow: 1,
-    color: 'green',
 },
 }
 
@@ -34,7 +33,7 @@ return (
     <IconButton edge="start" color="inherit" onClick={handleDetailClose} aria-label="close">
         <CloseIcon />
     </IconButton>
-    <Typography variant="h6" className={styles.title}>
+    <Typography variant="h6" className={classes.title}>
         {detail.Name}
     </Typography>
     <Button autoFocus color="inherit" onClick={handleDetailClose}>
