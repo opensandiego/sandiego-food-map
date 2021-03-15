607f84aa5d67820f29e1fc5ef06b4a4340532379
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2021-03-15 14:55:23 -0700

    changed ZoomControl from react-leaflet to the bottom right position to avoid dissapearing when set to bottom left.

--- src/index.jsx
+++ src/index.jsx
@@ -295,7 +295,7 @@ function FoodMap() {
         >
           <MyComponent center={position} />
 
-          <ZoomControl position="bottomleft" />
+          <ZoomControl position="bottomright" />
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
