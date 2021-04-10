7ac1b08a8a63f127125b84f66c16650c8b3c10ef
Bolun Thompson <abolunthompson@gmail.com>
2021-04-10 14:36:35 -0700

    Use useStyles instead of inline styles

--- src/components/FilterType.jsx
+++ src/components/FilterType.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: { marginLeft: 10 },
+});
 
 const soup_kitchen_regex = RegExp("/(?:)lunch|meal|breakfast|dinner/", "i");
 
@@ -10,8 +15,9 @@ const allSitesFilter = () => true;
 
 const FilterType = ({ addFilter }) => {
   const changeSiteFilter = (filterer) => addFilter("Description__c", filterer);
+  const classes = useStyles();
   return (
-    <span style={{marginLeft: 10}}>
+    <span className={classes.root}>
       <Button
         color="inherit"
         onClick={() => changeSiteFilter(soupKitchenFilter)}
